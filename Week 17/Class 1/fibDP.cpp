3d21b51f1936318a02a0722421677f984ed654ae
Anishka Kesaria <kescotak@gmail.com>
2023-08-12 17:49:40 +0530

    DP - 1 added

---
+++ Week 17/Class 1/fibDP.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Top Down
+
+class Solution {
+public:
+    
+    int findfib(vector<int> &dp, int n)
+    {
+        if(n==1 || n==0)
+            return n;
+        if(dp[n]==-1)
+            dp[n] = findfib(dp, n-1) + findfib(dp, n-2);
+        return dp[n];
+    }
+    
+    int fib(int n) {
+        //Step 1
+        vector<int> dp(n+1, -1);
+        return findfib(dp, n);
+    }
+};
