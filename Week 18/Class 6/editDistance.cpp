9cfeba97c37338e20566f6166e06573a68a4156c
Anishka Kesaria <kescotak@gmail.com>
2023-10-03 11:05:43 +0530

    DP - 6 added

---
+++ Week 18/Class 6/editDistance.cpp
@@ -0,0 +1,201 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solveUsingRec(string &word1, string &word2, int i, int j)
+    {
+        if(i>=word1.size())
+        {
+            if(j>=word2.size())
+                return 0;
+            else
+                return word2.size()-j;
+        }
+
+        if(j>=word2.size())
+        {
+            if(i>=word1.size())
+                return 0;
+            else
+                return word1.size()-i;
+        }
+
+        if(word1[i]==word2[j])
+            return solveUsingRec(word1, word2, i+1, j+1);
+
+        else
+        {
+            int insertOp = 1 + solveUsingRec(word1, word2, i, j+1);
+            int deleteOp = 1 + solveUsingRec(word1, word2, i+1, j);
+            int replaceOp = 1 + solveUsingRec(word1, word2, i+1, j+1);
+            return (min(insertOp, min(deleteOp, replaceOp)));
+        }
+    }
+
+    int solveUsingMem(string &word1, string &word2, int i, int j, vector<vector<int>> &dp)
+    {
+        if(i>=word1.size())
+        {
+            if(j>=word2.size())
+                return 0;
+            else
+                return word2.size()-j;
+        }
+
+        if(j>=word2.size())
+        {
+            if(i>=word1.size())
+                return 0;
+            else
+                return word1.size()-i;
+        }
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        int ans;
+
+        if(word1[i]==word2[j])
+            ans = solveUsingMem(word1, word2, i+1, j+1, dp);
+
+        else
+        {
+            int insertOp = 1 + solveUsingMem(word1, word2, i, j+1, dp);
+            int deleteOp = 1 + solveUsingMem(word1, word2, i+1, j, dp);
+            int replaceOp = 1 + solveUsingMem(word1, word2, i+1, j+1, dp);
+            ans = (min(insertOp, min(deleteOp, replaceOp)));
+        }
+
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+
+    int solveUsingTab(string word1, string word2)
+    {
+        int k = max(word1.size(), word2.size());
+        // vector<vector<int>> dp(k+2, vector<int> (k+2, 0));
+        vector<vector<int>> dp(word1.size()+1, vector<int> (word2.size()+1, 0));
+
+        for(int j=0; j<word2.size(); j++)
+        {
+            dp[word1.size()][j] = word2.size()-j;
+        }
+
+        for(int i=0; i<word1.size(); i++)
+        {
+            dp[i][word2.size()] = word1.size()-i;
+        }
+
+        // int n1 = word1.size();
+        // int n2 = word2.size();
+
+        // for(int i=n1; i<=k; i++)
+        // {
+        //     for(int j=0; j<=k; j++)
+        //     {
+        //         if(j>=n2)
+        //         {
+        //             dp[i][j] = 0;
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = n2-j;
+        //         }
+        //     }
+        // }
+
+        // for(int j=n2; j<=k; j++)
+        // {
+        //     for(int i=0; i<=k; i++)
+        //     {
+        //         if(i>=n1)
+        //             dp[i][j] = 0;
+        //         else
+        //             dp[i][j] = n1-i;
+        //     }
+        // }
+
+        for(int i=word1.size()-1; i>=0; i--)
+        {
+            for(int j=word2.size()-1; j>=0; j--)
+            {
+                int ans;
+
+                if(word1[i]==word2[j])
+                    ans = dp[i+1][j+1];
+
+                else
+                {
+                    int insertOp = 1 + dp[i][j+1];
+                    int deleteOp = 1 + dp[i+1][j];
+                    int replaceOp = 1 + dp[i+1][j+1];
+                    ans = (min(insertOp, min(deleteOp, replaceOp)));
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+    
+    int solveSO(string word1, string word2)
+    {
+        int k = max(word1.size(), word2.size());
+        vector<int> curr(k+1, 0);
+        vector<int> next(k+1, 0);
+        
+        for(int j=0; j<word2.size(); j++)
+        {
+            next[j] = word2.size()-j;
+        }
+        
+        for(int i=word1.size()-1; i>=0; i--)
+        {
+            curr[word2.size()] = word1.size()-i;
+            for(int j=word2.size()-1; j>=0; j--)
+            {
+                int ans;
+
+                if(word1[i]==word2[j])
+                    ans = next[j+1];
+
+                else
+                {
+                    int insertOp = 1 + curr[j+1];
+                    int deleteOp = 1 + next[j];
+                    int replaceOp = 1 + next[j+1];
+                    ans = (min(insertOp, min(deleteOp, replaceOp)));
+                }
+
+                curr[j] = ans;
+            }
+            
+            next = curr;
+        }
+        
+        return next[0];
+        
+    }
+
+    
+
+    int minDistance(string word1, string word2) {
+        if(word1.size()==0 || word2.size()==0)
+            return max(word1.size(), word2.size());
+
+        int i = 0;
+        int j = 0;
+
+        int k = max(word1.size(), word2.size());
+
+        vector<vector<int>> dp(k+1, vector<int> (k+1, -1));
+        // int ans = solveUsingMem(word1, word2, i, j, dp);
+        // int ans = solveUsingTab(word1, word2);
+        int ans = solveSO(word1, word2);
+
+
+        // int ans = solveUsingRec(word1, word2, i, j);
+        return ans;
+    }
+};
---
+++ Week 18/Class 6/longestCommonSubsequence.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solveUsingRec(string text1, string text2, int i, int j)
+    {
+        if(i>=text1.size() || j>=text2.size())
+            return 0;
+
+        //char matched
+        if(text1[i]==text2[j])
+        {
+            return (1+solveUsingRec(text1, text2, i+1, j+1));
+        }
+        //char not matched
+        else
+        {
+            return (max(solveUsingRec(text1, text2, i, j+1), solveUsingRec(text1, text2, i+1, j)));
+        }
+    }
+
+    int solveUsingMem(string &text1, string &text2, int i, int j, vector<vector<int>> &dp)
+    {
+        if(i>=text1.size() || j>=text2.size())
+            return 0;
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        int ans;
+
+        if(text1[i]==text2[j])
+            ans = (1+solveUsingMem(text1, text2, i+1, j+1, dp));
+        else
+            ans = max(solveUsingMem(text1, text2, i, j+1, dp), solveUsingMem(text1, text2, i+1, j, dp));
+
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+
+    int solveUsingTab(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+
+        for(int i=text1.size()-1; i>=0; i--)
+        {
+            for(int j=text2.size()-1; j>=0; j--)
+            {
+                int ans;
+                if(text1[i]==text2[j])
+                {
+                    ans = 1+dp[i+1][j+1];
+                }
+                else
+                {
+                    ans = max(dp[i+1][j], dp[i][j+1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    int solveSO(string &text1, string &text2)
+    {
+        int k = max(text1.size(), text2.size());
+
+        vector<int> curr (k+1, 0);
+        vector<int> next (k+1, 0);
+
+        for(int i=text1.size()-1; i>=0; i--)
+        {
+            for(int j=text2.size()-1; j>=0; j--)
+            {
+                int ans;
+
+                if(text1[i]==text2[j])
+                    ans = 1+next[j+1];
+                else
+                    ans = max(curr[j+1],next[j]);
+
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        // vector<vector<int>> dp(n, vector<int> (m,-1));
+        // int ans = solveUsingMem(text1, text2, 0, 0, dp);
+        // int ans = solveUsingTab(text1, text2);
+        int ans = solveSO(text1, text2);
+        // int ans = solveUsingRec(text1, text2, 0, 0);
+        return ans;
+    }
+};
